f393c4d42a9e9a257c2f3de84c08694259eeb4e6
Jakob Ferdinand Wegenschimmel <j.wegenschimmel@gmail.at>
2018-09-04 13:00:18 +0200

    Add Problem Sample

--- EfCoreManyToManyAddNewEntityProblemSample/EfCoreManyToManyAddNewEntityProblemSample/Program.cs
+++ EfCoreManyToManyAddNewEntityProblemSample/EfCoreManyToManyAddNewEntityProblemSample/Program.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace EfCoreManyToManyAddNewEntityProblemSample
 {
@@ -10,6 +8,57 @@ namespace EfCoreManyToManyAddNewEntityProblemSample
     {
         static void Main(string[] args)
         {
+            using (var context = new SampleDbContext())
+            {
+                context.Database.EnsureDeleted();
+                context.Database.EnsureCreated();
+
+                var teacher = context.CreateAddedEntity<Teacher>();
+                teacher.Name = "Sepp";
+                var student = context.CreateAddedEntity<Student>();
+                student.Name = "Luis";
+
+                var ts = context.CreateAddedEntity<Teacher_Student>();
+                ts.Teacher = teacher;
+                ts.Student = student;
+
+                context.SaveChanges();
+            }
+
+            Problem();
+            Console.WriteLine("Ready");
+            Console.ReadKey();
+        }
+
+        private static void Problem()
+        {
+            using (var context = new SampleDbContext())
+            {
+                var teacher = context.Teachers.FirstOrDefault();
+
+                var connections = context.Teacher_Students.ToList();
+                var student = context.CreateAddedEntity<Student>();
+
+                student.Name = "Alex";
+                var ts = context.CreateAddedEntity<Teacher_Student>();
+                ts.Teacher = teacher;
+                ts.TeacherId = teacher.Id;
+                ts.Student = student;
+                ts.StudentId = student.Id;
+
+                context.SaveChanges();
+            }
+        }
+    }
+
+    public static partial class Extensions
+    {
+        public static T CreateAddedEntity<T>(this DbContext context)
+            where T : class
+        {
+            var entity = Activator.CreateInstance<T>();
+            context.Add(entity);
+            return entity;
         }
     }
 }
